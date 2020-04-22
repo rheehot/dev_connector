7e8130ee62ca26ae6ae6dc3f31bac411c8d536c5
shjang7 <suhy.jang.shj@gmail.com>
2020-04-22 10:53:02 +0900

    Display and Delete comment

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -12,58 +12,56 @@ const PostItem = ({
   auth,
   post: { _id, text, name, avatar, user, likes, comments, date },
   showActions,
-}) => {
-  return (
-    <div className="post bg-white p-1 my-1">
-      <div>
-        <Link to={`/profile/${user}`}>
-          <img className="round-img" src={avatar} alt="" />
-          <h4>{name}</h4>
-        </Link>
-      </div>
-      <div>
-        <p className="my-1">{text}</p>
-        <p className="post-date">
-          Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
-        </p>
-        {showActions && (
-          <Fragment>
-            <button
-              type="button"
-              className="btn btn-light"
-              onClick={e => addLike(_id)}
-            >
-              <i className="fas fa-thumbs-up" />{' '}
-              <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
-            </button>
-            <button
-              type="button"
-              className="btn btn-light"
-              onClick={e => removeLike(_id)}
-            >
-              <i className="fas fa-thumbs-down" />
-            </button>
-            <Link to={`/post/${_id}`} className="btn btn-primary">
-              Discussion{' '}
-              {comments.length > 0 && (
-                <span className="comment-count">{comments.length}</span>
-              )}
-            </Link>
-            {!auth.loading && user === auth.user._id && (
-              <button
-                type="button"
-                className="btn btn-danger"
-                onClick={e => deletePost(_id)}
-              >
-                <i className="fas fa-times" />
-              </button>
-            )}
-          </Fragment>
-        )}
-      </div>
+}) => (
+  <div className="post bg-white p-1 my-1">
+    <div>
+      <Link to={`/profile/${user}`}>
+        <img className="round-img" src={avatar} alt="" />
+        <h4>{name}</h4>
+      </Link>
     </div>
-  );
-};
+    <div>
+      <p className="my-1">{text}</p>
+      <p className="post-date">
+        Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
+      </p>
+      {showActions && (
+        <Fragment>
+          <button
+            type="button"
+            className="btn btn-light"
+            onClick={e => addLike(_id)}
+          >
+            <i className="fas fa-thumbs-up" />{' '}
+            <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
+          </button>
+          <button
+            type="button"
+            className="btn btn-light"
+            onClick={e => removeLike(_id)}
+          >
+            <i className="fas fa-thumbs-down" />
+          </button>
+          <Link to={`/posts/${_id}`} className="btn btn-primary">
+            Discussion{' '}
+            {comments.length > 0 && (
+              <span className="comment-count">{comments.length}</span>
+            )}
+          </Link>
+          {!auth.loading && user === auth.user._id && (
+            <button
+              type="button"
+              className="btn btn-danger"
+              onClick={e => deletePost(_id)}
+            >
+              <i className="fas fa-times" />
+            </button>
+          )}
+        </Fragment>
+      )}
+    </div>
+  </div>
+);
 
 PostItem.defaultProps = {
   showActions: true,
